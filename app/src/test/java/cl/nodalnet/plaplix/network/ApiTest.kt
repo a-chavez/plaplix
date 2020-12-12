acac8624f300950e435d8cb7c16c78e84394582c
Antonio Chávez V <66529235+ElArroz@users.noreply.github.com>
2020-12-12 15:28:57 -0300

    Working: Testing
    -grade
    -apiTest
    -TestDao

---
+++ app/src/test/java/cl/nodalnet/plaplix/network/ApiTest.kt
@@ -0,0 +1,9 @@
+package cl.nodalnet.plaplix.network
+
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+
+class ApiTest {
+    lateinit var mMockWebServer: MockWebServer
+
+}
